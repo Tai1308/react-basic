d90015a3c1f629b94d35badac4d929d579a14662
Nguyến Tấn Tài <tantai13082001@gmail.com>
2024-11-06 16:35:34 +0700

    bai 2

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Tai",
+    adress: "Ha Noi",
+    age: "23",
+  };
+
   //JSX
   render() {
-    return <div> my first component</div>;
+    return <div> my name is{this.state.name}</div>;
   }
 }
 
