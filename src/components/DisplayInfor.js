072a82917bcbfb9b3ea8dc1daa57f79dd1e0ef68
Nguyến Tấn Tài <tantai13082001@gmail.com>
2024-11-07 14:08:55 +0700

    bai 8

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,13 +1,49 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowListUser: true,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser,
+    });
+  };
   render() {
-    console.log(this.props);
+    const { listUsers } = this.props;
+    console.log(listUsers);
+
     //props
     return (
       <div>
-        <div>My name is {this.props.name}</div>
-        <div>My age is {this.props.age}</div>
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowListUser === true
+              ? "Hide list users:"
+              : "Show list users:"}
+          </span>
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div
+                  key={user.id}
+                  className={
+                    +user.age >= 50 ? "blue" : +user.age > 18 ? "red" : "green"
+                  }
+                >
+                  <div>My name is {user.name}</div>
+                  <div>My age is {user.age}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,19 +1,37 @@
 import React from "react";
-import UserInfor from "./Userinfor";
+import AddUserInfor from "./AddUserinfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Nguyen Tan Tai", age: "17" },
+      { id: 2, name: " Tan Tai", age: "25" },
+      { id: 3, name: "Nguyen Tien Dung", age: "50" },
+    ],
+  };
+
+  handleAddNewUser = (userObj) => {
+    // Cách 1
+    // let listUsersNew = this.state.listUsers;
+    // listUsersNew.unshift(userObj);
+    // this.setState({
+    //   listUsers: listUsersNew,
+    // });
+
+    this.setState({
+      listUsers: [userObj, ...this.state.listUsers], // dùng để cập nhật ở đầu mảng, dấu 3 chấm copy lại trạng thái của bảng listUsers
+      //   listUsers: [ ...this.state.listUsers,userObj], // dùng để cập nhật ở cuối mảng
+    });
+  };
   //JSX
   render() {
-    const myInfor = ["a", "b", "c"];
     return (
       <div>
-        <UserInfor />
+        <AddUserInfor handleAddNewUser={this.handleAddNewUser} />
         <br />
         <br />
-        <DisplayInfor name="Nguyen Tan Tai" age="24" />
-        <hr />
-        <DisplayInfor name="Tan Tai" age={24} myInfor={myInfor} />
+        <DisplayInfor listUsers={this.state.listUsers} />
       </div>
     );
   }
